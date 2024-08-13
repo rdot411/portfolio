6f407eaf2292b1c2acce19c9bb04a1295187a8c2
rdot411 <167403324+rdot411@users.noreply.github.com>
2024-08-13 18:12:20 +1200

    new project added + grammar

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -3,7 +3,7 @@ import styles from './ProjectStyles.module.css';
 import pokedex from '../../assets/pokedex.png';
 import freshBurger from '../../assets/fresh-burger.png';
 import hipsster from '../../assets/hipsster.png';
-import fitlift from '../../assets/fitlift.png';
+import rbsLogo from '../../assets/rbslogo.png';
 import restrauntTemp from '../../assets/RestrauntLogo.png';
 import ProjectCard from '../../common/ProjectCard';
 
@@ -14,10 +14,9 @@ function Projects() {
     <h1 className="sectionTitle">Projects</h1>
     <section id='projects' className={styles.container}>
       <div className={styles.projectsContainer}>
-        <ProjectCard src={pokedex} link='https://whosthatmon.netlify.app/' h3='WhosThatMon' p='Pokedex Project using HTML, CSS, JavaScript and PokeApi'/>
-        <ProjectCard src={restrauntTemp} link='https://restemplate.netlify.app/' h3='RestTemplate' p='Frontend restraunt template using HTML, CSS, React and Tailwind CSS'/>
-        {/*<ProjectCard src={hipsster} link='https://github.com/' h3='hipssterr' p='glasses'/>
-        <ProjectCard src={fitlift} link='https://github.com/' h3='fitlift' p='fitness shop'/>*/}
+        <ProjectCard src={pokedex} link='https://whosthatmon.netlify.app/' h3='WhosThatMon' p='Pokedex Project using HTML, CSS, JavaScript and PokeApi.'/>
+        <ProjectCard src={restrauntTemp} link='https://restemplate.netlify.app/' h3='RestTemplate' p='Frontend restraunt template using HTML, CSS, React and Tailwind CSS.'/>
+        <ProjectCard src={rbsLogo} link='https://bustemplate.netlify.app/' h3='Business Website Template' p='Frontend business/recruitment website made using HTML, CSS, React and Tailwind CSS. More improvments + backend coming soon.'/>
       </div>
     </section>
     </>
