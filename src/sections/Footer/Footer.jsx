3705f387236acffe8103a3341910d9207e2bc27c
rdot411 <167403324+rdot411@users.noreply.github.com>
2025-07-27 07:53:03 +1200

    Update Footer.jsx

--- src/sections/Footer/Footer.jsx
+++ src/sections/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import styles from './FooterStyles.module.css';
 function Footer() {
   return (
     <section id='footer' className={styles.container}>
-        <p>© 2024 Raghav Tiwari.<br/>
+        <p>© 2025 Raghav Tiwari.<br/>
         All rights reserved.</p>
 
     </section>
